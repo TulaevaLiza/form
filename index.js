0ebfc6b7e7a5e06f451c01ed8af786b4aee2b40b
TulaevaLiza <pankova_liza@mail.ru>
2017-08-02 20:58:46 +0300

    создание объекта MyForm

---
+++ index.js
@@ -0,0 +1,22 @@
+var MyForm={
+validate: function() {
+}
+getData: function() {
+	this.fio=document.getElementById("fio");
+	this.email=document.getElementById("email");
+	this.phone=document.getElementById("phone");
+}
+setData: function(fio,email,phone) {
+}
+submit: function() {
+}
+display: function() {
+	console.log(this.fio);
+	console.log(this.email);
+	console.log(this.phone);
+}
+}
+
+MyForm.getData();
+MyForm.display();
+
