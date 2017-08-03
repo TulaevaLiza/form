38957817656daeb1e97c04b0239c2b83c44a6bcb
TulaevaLiza <pankova_liza@mail.ru>
2017-08-03 13:45:26 +0300

    добавление проверки поля ФИО

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-var MyForm={
-validate: function() {
-}
-getData: function() {
-	this.fio=document.getElementById("fio");
-	this.email=document.getElementById("email");
-	this.phone=document.getElementById("phone");
-}
-setData: function(fio,email,phone) {
-}
-submit: function() {
-}
-display: function() {
-	console.log(this.fio);
-	console.log(this.email);
-	console.log(this.phone);
-}
-};
-
-MyForm.getData();
-MyForm.display();
-
+$(document).ready(function() {
+	var error=();
+	$('#fio').val("");
+	$('#fio').change(function() {
+		if($(this).next().hasClass('error')) {
+			$(this).next().remove();
+		}
+		$error['fio']=0;
+		var fio=$(this).val().replace(/\s*$/,"").replace(/^\s*/,"");
+		var x=fio.split(" ");
+		if(x.length!=3) {
+			$(this).after("<div class='bg-warning error'>ФИО должно содержать три слова</div>");
+			$error['fio']=1;
+		}
+	});
+});
