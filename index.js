9280daf9688cd903275e43c71a00593fe7ca9864
TulaevaLiza <pankova_liza@mail.ru>
2017-08-20 15:56:25 +0300

    добавлены методы validate и submit

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-	var emailtmpl = /^[a-zA-Z0-9_\-\.]+@(ya\.ru|yandex\.ru|yandex\.ua|yandex\.by|yandex\.kz|yandex\.com)$/;
-	var phonetmpl = /^\+7\(\d\d\d\)\d\d\d-\d\d-\d\d$/;
 	var error_fio =0, error_email = 0, error_phone =0, error = 0 ;
 	var url="";
 
@@ -9,19 +7,93 @@ $(document).ready(function() {
 			this.fio=$("#fio");
 			this.email=$("#email");
 			this.phone=$("#phone");
-			this.submit=$("#submitButton");
+			this.submitButton=$("#submitButton");
+
+			this.fio.removeClass('error');
+			this.email.removeClass('error');
+			this.phone.removeClass('error');
 			console.log(this);
 		},
-		clearForm: function () {
-			this.fio.val("");
-			this.email.val("");
-			this.phone.val("");
-			this.submit.removeAttr("disabled");	
+		validate: function () {
+			var emailtmpl = /^[a-zA-Z0-9_\-\.]+@(ya\.ru|yandex\.ru|yandex\.ua|yandex\.by|yandex\.kz|yandex\.com)$/;
+			var phonetmpl = /^\+7\s*\(\d\d\d\)\s*\d\d\d-\d\d-\d\d$/;
+			var obj = {'isValid': true, 'errorFields':[]};
+
+			var x=this.fio.val().trim().split(" ");
+			if(x.length!=3) {
+				obj.isValid=false;	
+				obj.errorFields.push('fio');
+			}			
+
+			if(!emailtmpl.test(this.email.val().trim())) {
+				obj.isValid=false;	
+				obj.errorFields.push('email');
+			}
+
+			if(!phonetmpl.test(this.phone.val().trim())) {
+				obj.isValid=false;	
+				obj.errorFields.push('phone');
+				console.log('phone '+this.phone.val().trim());
+			}
+			else {
+				let ar = this.phone.val().split(''), sum=0;
+				for(let i in ar) {
+					if(Number(ar[i]))
+						sum+=Number(ar[i]);
+				}
+				console.log('sum = '+sum);
+				if(sum>30) {
+					obj.isValid=false;	
+					obj.errorFields.push('phone');
+				}
+			}
+			
+			return obj;
+		},
+		setData: function(f) {
+			this.fio.val(f.fio);
+			this.phone.val(f.phone);
+			this.email.val(f.email);
+			this.submitButton.removeAttr("disabled");
+		},
+		submit: function() {
+			let validRes=this.validate();			
+			console.log(validRes);
+			if(!validRes.isValid) {
+				for(let i in validRes.errorFields) {
+						this[validRes.errorFields[i]].addClass("error");	
+				}
+			}
+			else {
+				this.submitButton.attr("disabled","disabled");
+				/*var xhttp=new XMLHttpRequest();
+				xhttp.open("GET","success.json",true);
+				xhttp.send();  
+				console.log(xhttp);		*/		
+				$.ajax({		
+					url: "/success.json",
+					cache: false,
+					success: function(data) {
+					//	$("#results").html(data);
+						console.log("Успех!");
+					},
+					error:  function(xhr, str){
+						console.log("Возникла ошибка!");
+						console.log(xhr);	
+					}
+				});
+			}
 		}
 	}
-
 MyForm.getForm();
-MyForm.clearForm();
+MyForm.setData({"fio":"","phone":"","email":""});
+
+$('#submitButton').click(function() {
+	MyForm.getForm();
+	MyForm.submit();	
+});
+
+
 /*
 	$('#fio').val("");
 	$('#email').val("");
